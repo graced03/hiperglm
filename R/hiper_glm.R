1487f9cca63c22ea4f8b160ecc250a94e10c8244
graced03 <dxy_03@outlook.com>
2023-02-10 23:55:51 -0500

    corrected the hiper_glm function - the model choices

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -2,21 +2,21 @@
 #'
 #' @param design the design matrix
 #' @param outcome the outcome vector/matrix of the model
-#' @param supported_model choose the type of GLM
+#' @param model choose the type of GLM
 #'
 #' @return S3 object of the model fitting output
 #'
 #' @export
 #'
-hiper_glm <- function(desgin, outcome, supported_model = "linear"){
+hiper_glm <- function(desgin, outcome, model = "linear"){
   # init_coef can be an optional argument for more advanced users
-  supporeted_model <- c("linear","logit")
+  supported_model <- c("linear","logit")
   if (!model %in% supported_model){
-    stop(sprintf("The model %s is not supported!", supported_model))
+    stop(sprintf("The model %s is not supported!", model))
   }
   # TODO: find MLE
   hglm_out <- list()
   class(hglm_out) <- "hglm" # turn it into an S3 object
-  warning("hglm)out is not yet implemented.")
+  warning("hglm out is not yet implemented.")
   return(hglm_out)
 }
